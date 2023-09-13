666b8df7e60bd44d2d4195348323b57aa044defc
taica84 <taica_rules@yahoo.com>
2023-09-13 21:22:55 +0300

    shopping list js

--- js/shoppinglist.js
+++ js/shoppinglist.js
@@ -6,53 +6,123 @@ const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 
-//--------------------  add items to the list ----------
-function getForm(e) {
-  e.preventDefault();
+const clearBtn = document.querySelector('.btn-clear');
+const filter = document.querySelector('.filter');
 
-  const newItem = itemInput.value;
+//--------------  display saved items from local storege
+
+//--------------------  add items to the list ----------
+function addItem(e) {
+  e.preventDefault();
+  
+  let newItem = itemInput.value;
 
   if (newItem === '') {
-    alert('Please add an item');
-    return;
+    alert('Please add an item!')
+    return
   }
 
-  const li = document.createElement('li');
-  li.appendChild(document.createTextNode(newItem));
+  addItemToDOM(newItem);
+  
+ itemInput.value = '';
 
-  const button = createButton('remove-item btn-link text-red');
-  li.appendChild(button);
-
-  itemList.appendChild(li)
-
-  console.log(li);
 }
 
-//---------- create elements inside 'li' -----
+//------TEMPLATE ------------------
+
+  //  <li>
+  //    Orange Juice
+  //    <button class="remove-item btn-link text-red">
+  //      <i class="fa-solid fa-xmark"></i>
+  //    </button>
+  //  </li>;
+
+//--------------------Add item to DOM
+
+function addItemToDOM(item) {
+
+  let li = document.createElement('li')
+  li.appendChild(document.createTextNode(item))
+  let btnEl = createButton('remove-item btn-link text-red');
+  li.appendChild(btnEl)
+
+  //Add li to the DOM
+  itemList.appendChild(li)
+
+  //Reset UI
+  resetUI()
+
+  console.log(itemList);
+}
+
+//---------- create elements inside 'li'(button and icon) ----
 
 function createButton(classes) {
-  const button = document.createElement('button');
-  button.className = classes;
-
-  const icon = createIcon('fa-solid fa-xmark');
-  button.appendChild(icon);
-  return button;
+  let btnEl = document.createElement('button')
+  btnEl.className = classes;
+  let icon = createIcon('fa-solid fa-xmark');
+  btnEl.appendChild(icon)
+  return btnEl
 }
 
 function createIcon(classes) {
-  const icon = document.createElement('i');
+  let icon = document.createElement('i')
   icon.className = classes;
-
-  return icon;
+  return icon
 }
 
-//   <li>
-//   Apples
-//   <button class="remove-item btn-link text-red">
-//     <i class="fa-solid fa-xmark"></i>
-//   </button>
-// </li>
+//---------------------filter items -----------
+
+//------------------  remove item --------
+
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains('remove-item')) {
+    alert('Are you sure?')
+    e.target.parentElement.parentElement.remove()
+      resetUI();
+
+  }
+}
+
+//-----------------  clear all items from list
+
+function clearItems() {
+  // ----solution 1 (professional)-----
+
+   while (itemList.firstChild) {
+     itemList.removeChild(itemList.firstChild)
+  }
+  
+  resetUI()
+
+  //----solution 2 (no professional)---
+
+  // itemList.innerHTML = '';
+}
+
+//-------resetUi (when is not any items in the list , hide filter and clear button) ------------
+
+function resetUI() {
+  const items = itemList.querySelectorAll('li');
+  if (items.length === 0) {
+    clearBtn.classList.add('hide')
+    filter.classList.add('hide');
+  } else {
+      clearBtn.classList.remove('hide');
+      filter.classList.remove('hide');
+  }
+}
 
 //---------------eventListeners -------------
 
-itemForm.addEventListener('submit', getForm);
+function init() {
+  itemForm.addEventListener('submit', addItem);
+   itemList.addEventListener('click', removeItem);
+   clearBtn.addEventListener('click', clearItems);
+  // filter.addEventListener('input', filterItems);
+  // document.addEventListener('DOMContentLoaded', displayItems);
+
+  resetUI();
+}
+
+init();
