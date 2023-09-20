0d1f8894614f27ad2d07b3df1c44f14b39a3c0cc
taica84 <taica_rules@yahoo.com>
2023-09-20 21:30:29 +0300

    edit links

--- projects/flixx-app/js/script.js
+++ projects/flixx-app/js/script.js
@@ -9,31 +9,28 @@ console.log(global.currentPage);
 function highlightActiveLink() {
   const links = document.querySelectorAll('.nav-link');
   links.forEach((link) => {
-    if (link.getAttribute('href') === global.currentPage) {
+    if (link.getAttribute('href') === global.currentPage.slice(20)) {
       link.classList.add('active');
     } 
-   if(global.currentPage === 'home.html') {
-      document.querySelector('.logo a').classList.add('active');
-    }
   });
 }
 
 function init() {
   switch (global.currentPage) {
     case `/`:
-    case `home.html`:
+    case `/projects/flixx-app/home.html`:
       console.log('Home');
       break;
-    case `shows.html`:
+    case `/projects/flixx-app/shows.html`:
       console.log('Shows');
       break;
-    case `movie-details.html`:
+    case `/projects/flixx-app/movie-details.html`:
       console.log('Movie-Details');
       break;
-    case `tv-details.html`:
+    case `/projects/flixx-app/tv-details.html`:
       console.log('Tv-Details');
       break;
-    case `search.html`:
+    case `/projects/flixx-app/search.html`:
       console.log('Search');
       break;
   }
